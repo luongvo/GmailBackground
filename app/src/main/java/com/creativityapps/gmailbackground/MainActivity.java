b1f8652e3ed4a2c1b557412d8a251f285ee1cdd7
Luong Vo <vodailuong@gmail.com>
2017-11-21 01:12:27 +0700

    [c] add sending attachment example

--- app/src/main/java/com/creativityapps/gmailbackground/MainActivity.java
+++ app/src/main/java/com/creativityapps/gmailbackground/MainActivity.java
@@ -1,15 +1,23 @@
 package com.creativityapps.gmailbackground;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.creativityapps.gmailbackgroundlibrary.BackgroundMail;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -23,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendTestEmail();
+                MainActivityPermissionsDispatcher.sendTestEmailWithPermissionCheck(MainActivity.this);
             }
         });
     }
@@ -50,7 +58,17 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void sendTestEmail() {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void sendTestEmail() {
+        // create a `Test.txt` and locate it on device sdcard and run the example
+        String fileName = Environment.getExternalStorageDirectory().getPath() + "/Test.txt";
+
         BackgroundMail.newBuilder(this)
                 .withUsername("username@gmail.com")
                 .withPassword("password12345")
@@ -60,7 +78,9 @@ public class MainActivity extends AppCompatActivity {
                 .withMailBcc("bcc-email@gmail.com")
                 .withSubject("this is the subject")
                 .withBody("this is the body")
+                .withAttachments(fileName)
                 .withUseDefaultSession(false)
+                .withProcessVisibility(true)
                 .withOnSuccessCallback(new BackgroundMail.OnSuccessCallback() {
                     @Override
                     public void onSuccess() {
